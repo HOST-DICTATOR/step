a6ab20b34807818ac82cda64b50e1b91c3d1605f
CHASOVSHIK <fore@mail.ru>
2020-11-15 14:22:58 +0300

    Добавьте файлы проекта.

---
+++ GIT_Visual/GIT_Visual.cpp
@@ -0,0 +1,43 @@
+﻿#include <cassert>
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <sstream>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+char* append(char* pStr, const char* pAddStr)
+{
+    // Проверить, что указатели ненулевые
+    assert(pStr != 0);
+    assert(pAddStr != 0);
+    // Код добавления pAddStr к pStr...
+
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    
+
+
+   
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
