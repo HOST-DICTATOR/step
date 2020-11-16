e7805b13c72207dd8c44f9a9508e06bbc03f69de
CHASOVSHIK <fore@mail.ru>
2020-11-16 15:26:55 +0300

    2

---
+++ GIT_Visual/DebugStuff.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef _DEBUG
+#define CONSTRUCTOR_TRACE   // Трассировка вызовов конструктора
+#define FUNCTION_TRACE     // Трассировка вызовов функции
+#endif // _DEBUG
+
+
+
+
+
+
