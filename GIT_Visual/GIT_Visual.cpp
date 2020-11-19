647960094105ff3ecfc612d57425e5d464e89933
CHASOVSHIK <fore@mail.ru>
2020-11-19 16:07:46 +0300

    8

--- GIT_Visual/GIT_Visual.cpp
+++ GIT_Visual/GIT_Visual.cpp
@@ -20,16 +20,7 @@ int main(int argc, char* argv[])
         cout << endl;
         cout << endl;
 
-<<<<<<< HEAD
 
-=======
-//Пошли вы на хуй!!
-//Ушлепаны!!
-//Чумари!
-//Orlando
-//Floor
-//So
->>>>>>> v1.1
 	return 0;
 }
 
