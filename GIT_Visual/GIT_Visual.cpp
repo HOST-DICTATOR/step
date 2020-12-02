bff114ab6076e39fa766f960e0dd20402d3df1a6
CHASOVSHIK <fore@mail.ru>
2020-12-02 14:02:41 +0300

    Secondary

--- GIT_Visual/GIT_Visual.cpp
+++ GIT_Visual/GIT_Visual.cpp
@@ -7,19 +7,7 @@ using namespace std;
 //solo
 int main(int argc, char* argv[])
 {
-	Name myName("Ivor", "Horton");                 // Try a single object
-
-	// Retrieve and store the name in a local char array  
-	char theName[10];
-	cout << "\nThe name is " << myName.getName(theName);
-
-	// Store the name in an array in the free store
-	char* pName = new char[myName.getNameLength()];
-	cout << "\nThe name is " << myName.getName(pName);
-
-	cout << endl;
-        cout << endl;
-        cout << endl;
+	
 
 
 	return 0;
